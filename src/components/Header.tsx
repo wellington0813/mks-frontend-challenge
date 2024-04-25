0f4c3be5bb7acae5959cd5985caa2ac974c9f56b
wellington0813 <2607wellington@gmail.com>
2024-04-25 08:03:01 -0300

    Feat: configuraro yarn realizado criação Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <section className="fixed w-full h-24 bg-custom-blue flex justify-between items-center z-40">
+            <div>
+
+            </div>
+        </section>
+    )
+}
