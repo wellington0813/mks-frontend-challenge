4047e5c8b9a8eab796ec8b8b68c5bd9f94cff0a3
wellington0813 <2607wellington@gmail.com>
2024-04-25 14:53:13 -0300

    Feat: Finalizado header dando inicio ao Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero(){
+    return(
+        <section className="pt-48 pb-24">
+            <div className=" max-w-7xl px-4 mx-auto">
+                <p className="text-black">AAAAAAAAAAAAAAAAAAAAA </p>
+            </div>
+        </section>
+    )
+        
+}
