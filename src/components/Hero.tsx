f2b6033425f01238ec717ffaa7b3c9c377d9e234
wellington0813 <2607wellington@gmail.com>
2024-04-26 14:55:26 -0300

    Feat: realizado ajustes nos cards de produtos

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,20 +6,19 @@ import { useState, useEffect } from 'react';
 function LoadingShimmer() {
   return (
     <div className="loading-shimmer">
-        loading ...
+        <p className="text-black font-bold text-2xl">
+            loading ...
+        </p>
     </div>
   );
 }
 
-// Exporte sua função Hero
 export function Hero() {
     const [loading, setLoading] = useState(true);
     const [dadosPrudutos, setDadosProdutos] = useState({});
     const { products } = dadosPrudutos
-
     console.log(products)
     useEffect(() => {
-        // Função para buscar os dados da API
         async function fetchData() {
             try {
                 const response = await axios.get('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC');
@@ -31,8 +30,6 @@ export function Hero() {
                 console.error('Erro ao buscar dados da API:', error);
             }
         }
-
-        // Chame a função para buscar os dados da API
         fetchData();
     }, []);
 
@@ -42,21 +39,32 @@ export function Hero() {
     return(
         <section className="pt-48 pb-24">
             <div className=" max-w-7xl px-4 mx-auto">
-                <div className="justfy-center lg:grid-cols-4 sm:grid-cols-2 grid grid-cols-1 gap-5">
+                <div className="justfy-center lg:grid-cols-4 sm:grid-cols-2 grid grid-cols-1 gap-7">
                     {
                         products.map(item => (
-                            <div className="w-full max-w-sm bg-white rounded-lg shadow-md" key={item.id}>
+                            <div className="flex flex-col justify-between w-full max-w-sm bg-white rounded-lg shadow-md" key={item.id}>
                                 <div className="p-3">
-                                    <Image className="p-5 rounded-t-lg"  src={item.photo}  alt={item.name}  width={250} height={150}/>
-                                    <div className=" grid grid-cols-2">
-                                        <p>
-                                            {item.name}
-                                        </p>
-                                        <div className="bg-[#373737] p-1 rounded-lg">
-                                            <p className="font-bold text-white text-center">R$ {item.price}</p>
+                                    <div className="">
+                                        <Image className="p-5 mx-auto"  src={item.photo}  alt={item.name}  width={250} height={150}/>
+                                    </div>
+                                    <div className="flex justify-between align-top">
+                                        <div className=" "> 
+                                            <p className="text-xl font-normal">
+                                                {item.name}
+                                            </p>
+                                        </div>
+                                        <div className=" bg-[#373737] rounded-lg p-3 flex items-center justify-center">
+                                            <p className="font-bold text-white text-sm text-center">R$ {item.price}</p>
                                         </div>
                                     </div>
+
+                                    <p className="text-black text-sm mt-5">
+                                        {item.description}
+                                    </p>
                                 </div>
+
+                                
+                               
                                 <button className="inline-block w-full p-2 rounded-b-lg bg-custom-blue hover:bg-blue-700 focus:outline-none">
                                     <p className="text-white text-xl"> COMPRAR</p>
                                 </button>
@@ -70,7 +78,6 @@ export function Hero() {
     )
 }
 
-// Use o hook useClient para marcar a função Hero como um componente do lado do cliente
 export default function ClientHero() {
   return <Hero />;
 }
